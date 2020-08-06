52b662b8a89ad32378ab68278fcb250e62d49e48
Hudson8811 <hudson88@yandex.ru>
2020-08-06 19:53:12 +0300

    changed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,10 +6,21 @@ $(document).ready(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 50 && !$(".header").hasClass("active"))
       $(".header").addClass("scrollable");
-    else $(".header").removeClass("scrollable");
+    else {
+      $(".header").removeClass("scrollable");
+      $(".header").removeClass("active");
+    }
   });
 
   $(document).on("click", ".header__burger", function () {
-    $(".header").toggleClass("active");
+    if (!$(".header").hasClass("active")) {
+      $(".header").addClass("active");
+    } else {
+      $(".header").removeClass("active");
+
+      if ($(window).scrollTop() > 50) {
+        $(".header").addClass("scrollable");
+      }
+    }
   });
 });
