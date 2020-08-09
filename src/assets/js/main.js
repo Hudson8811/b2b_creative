6595c53c39192226c94682311a026f0dd82cf221
Hudson8811 <hudson88@yandex.ru>
2020-08-09 13:54:52 +0300

    chanegd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -36,4 +36,63 @@ $(document).ready(function () {
       $(".services__text").slideUp();
     }
   });
+
+  $(document).on("focus", ".turnkey_form__label input", function () {
+    $(this).parent().addClass("active");
+    $(this).parent().removeClass("error");
+  });
+
+  $(document).on("blur", ".turnkey_form__label input", function () {
+    if ($(this).val() == "") {
+      $(this).parent().removeClass("active");
+      $(this).parent().addClass("error");
+    } else {
+      $(this).parent().removeClass("error");
+      if (
+        $(this).attr("type") == "email" &&
+        !isValidEmailAddress($(this).val())
+      ) {
+        $(this).parent().addClass("error");
+      }
+    }
+  });
+
+  $(document).on("change", "#file1_input", function () {
+    $("#turnkey_form__file__btn_wrapper1").removeClass("active");
+    $("#turnkey_form__file__btn_wrapper2").addClass("active");
+    $("#turnkey_form__file__content1").addClass("active");
+  });
+
+  $(document).on("change", "#file2_input", function () {
+    $("#turnkey_form__file__content2").addClass("active");
+    $(".turnkey_form__file__btn_wrapper").addClass("disabled");
+  });
+
+  $(document).on(
+    "click",
+    "#turnkey_form__file__content1 .turnkey_form__file__content_btn__remove",
+    function () {
+      $(this)
+        .parent(".turnkey_form__file__content_btns")
+        .parent("#turnkey_form__file__content1")
+        .removeClass("active");
+      $("#turnkey_form__file__btn_wrapper1").addClass("active");
+      $("#turnkey_form__file__btn_wrapper2").removeClass("active");
+      $(".turnkey_form__file__btn_wrapper").removeClass("disabled");
+    }
+  );
+
+  $(document).on(
+    "click",
+    "#turnkey_form__file__content2 .turnkey_form__file__content_btn__remove",
+    function () {
+      $(this)
+        .parent(".turnkey_form__file__content_btns")
+        .parent("#turnkey_form__file__content2")
+        .removeClass("active");
+      $("#turnkey_form__file__btn_wrapper1").removeClass("active");
+      $("#turnkey_form__file__btn_wrapper2").addClass("active");
+      $(".turnkey_form__file__btn_wrapper").removeClass("disabled");
+    }
+  );
 });
